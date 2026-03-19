54e1d1f84b42cf02eaab35041ec2910e81fc9806
Mohammad Haris Ali <mohammedharisali786@gmail.com>
2026-03-19 21:41:50 +0000

    adding loggin and removing test appolication

--- task-management/src/main/java/uk/gov/hmcts/taskmanagement/controller/TaskController.java
+++ task-management/src/main/java/uk/gov/hmcts/taskmanagement/controller/TaskController.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import uk.gov.hmcts.taskmanagement.models.dto.TaskRequestDTO;
@@ -16,6 +17,7 @@ import static org.springframework.http.HttpStatus.CREATED;
 
 @RequestMapping("/tasks")
 @RestController
+@Slf4j
 @RequiredArgsConstructor
 @Tag(name = "Tasks", description = "Task management endpoints")
 public class TaskController {
@@ -25,6 +27,7 @@ public class TaskController {
   @GetMapping()
   @Operation(summary = "Get all tasks", description = "Returns a list of all tasks")
   public ResponseEntity<List<TaskResponseDTO>> getAllTasks(){
+    log.info("Received request for all tasks");
     return ResponseEntity.ok(taskService.getAllTasks());
   }
 
@@ -32,6 +35,7 @@ public class TaskController {
   @Operation(summary = "Get a singular task by ID",
       description = "Returns one task based on the ID provided and if found, or code 404 if the task is not found")
   public ResponseEntity<TaskResponseDTO> getTasksById(@PathVariable Long id){
+    log.info("Received request for task id: {}", id);
     return ResponseEntity.ok(taskService.getTaskById(id));
   }
 
@@ -39,6 +43,7 @@ public class TaskController {
   @Operation(summary = "Creates new task",
       description = "Returns the newly created object based on the provided taskRequestDTO")
   public ResponseEntity<TaskResponseDTO> createNewTask(@Valid @RequestBody TaskRequestDTO taskRequestDTO){
+    log.info("Received request for new task creation: {}", taskRequestDTO);
     return ResponseEntity.status(CREATED).body(taskService.createTask(taskRequestDTO));
   }
 
@@ -46,6 +51,7 @@ public class TaskController {
   @Operation(summary = "Delete a task by ID",
       description = "Returns No content for entity deletion, or code 404 if the task is not found")
   public ResponseEntity<Void> deleteTask(@PathVariable Long id){
+    log.info("Received request for task deletion, id: {}", id);
     taskService.deleteTask(id);
     return ResponseEntity.noContent().build();
 
@@ -54,6 +60,7 @@ public class TaskController {
   @PatchMapping("/{id}")
   @Operation(summary = "Update a task", description = "Returns an updated task based on the updated taskRequesDTO 200, or code 404 if the task is not found")
   public ResponseEntity<TaskResponseDTO> updateTask(@PathVariable Long id, @Valid @RequestBody TaskRequestDTO taskRequestDTO){
+    log.info("Received request for updated task information, id: {}", id);
     return ResponseEntity.ok(taskService.updateTask(taskRequestDTO, id));
   }
 }
